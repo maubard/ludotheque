3e3fe5605190c23e0e8144320c2b6f85bc640b1e
maubard <marion.digiacomo@gmail.com>
2018-07-23 21:33:12 +0200

    Init project

---
+++ src/main/java/com/maubard/ludotheque/LudothequeApplication.java
@@ -0,0 +1,12 @@
+package com.maubard.ludoth.que;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LudothèqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LudothèqueApplication.class, args);
+	}
+}
---
+++ src/test/java/com/maubard/ludotheque/LudothequeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.maubard.ludoth.que;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LudothèqueApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
